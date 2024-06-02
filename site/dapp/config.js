48c2875346f23df42f10b863783bd111553ee665
root <real0rouse@gmail.com>
2024-06-02 21:10:20 +0200

    change to new fresh contracts

--- site/dapp/config.js
+++ site/dapp/config.js
@@ -1,7 +1,7 @@
 
-const contract1Address = '0x2603b98C773db4FbF1C894DD9aFb06B795F20584'; // Replace with actual Contract 1 address
-const contract2Address = '0x451FFa8101256dC0a6E257bc6e3dC79db6a4D2F8'; // Replace with actual Contract 2 address
-const tokenContractAddress = '0x158883445E27945bD0f68b274D947132C8fed57B';
+const contract1Address = '0x120445161DdF9071794Fd938AFa00a73f4547c1d'; // Replace with actual Contract 1 address
+const contract2Address = '0x19bb5C08aB1582928AE99714D21903f015D3fDcD'; // Replace with actual Contract 2 address
+const tokenContractAddress = '0x743A3B045DD8a5e6799b51B13438CEbdaea7FAA5';
 // DEPRECATED - const rewardsReserveContractAddress = '0x948b5A9C7BefcF6Fe336278B968c8Db442a34ed0';
 
 // console.log("config.js is starting...");
